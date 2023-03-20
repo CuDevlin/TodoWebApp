2018b8d478933295ab5e2c17697412d7bbe2e404
Cudevlin <popcanindustries@gmail.com>
2023-03-20 16:47:41 +0100

    view todos now works

--- HttpClients/ClientInterfaces/ITodoService.cs
+++ HttpClients/ClientInterfaces/ITodoService.cs
@@ -1,8 +1,11 @@
 ﻿using Shared.DTOs;
+using Shared.Models;
 
 namespace HttpClients.ClientInterfaces;
 
 public interface ITodoService
 {
     Task CreateAsync(TodoCreationDto dto);
+
+    Task<ICollection<Todo>> GetAsync(string? userName, int? userId, bool? completedStatus, string? titleContains);
 }
